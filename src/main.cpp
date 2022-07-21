70ad91e456e13874e4a366c883d60eaeb03bbc9c
shecannotsee <shecannotsee@163.com>
2022-07-21 17:02:56 +0800

    调整jenkins

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
-
+#include <sheLog/sheLog.hpp>
 
 int main() {
+  sheLog::SheLog log("./","myLog");
+  log.logMessage(sheLog::logLevel::INFO,"message");
   std::cout<<"hello sheDB\n";
   return 0;
 }
