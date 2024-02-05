f0400f92f059906ff071e8efed2a155648c0cd83
shecannotsee <shecannotsee@163.com>
2024-02-05 14:05:07 +0800

    [build] Add new cmake files

---
+++ cmake/import.cmake
@@ -0,0 +1,17 @@
+
+########################################################################################################################
+# Import lib                                                                                                           #
+########################################################################################################################
+
+# include
+include_directories(${CMAKE_SOURCE_DIR}/third_party/she_test/include)
+
+# lib
+link_directories()
+
+# Internal project
+include_directories(${CMAKE_SOURCE_DIR}/src)
+include_directories(${CMAKE_SOURCE_DIR}/test)
+include_directories(${CMAKE_SOURCE_DIR}/exec)
+
+
---
+++ cmake/she_db-config.cmake
@@ -0,0 +1,13 @@
+
+# ${she_db_FOUND}       : right get
+# ${she_db_INCLUDE_DIR} : include
+# ${she_db_LIBRARIES}   : lib
+# ${she_db_LINK_TARGET} : link target
+
+set(she_db "she_db")
+set(${she_db}_FOUND "1")
+set(${she_db}_INCLUDE_DIR   "${CMAKE_PREFIX_PATH}/include")
+set(${she_db}_LIBRARIES     "${CMAKE_PREFIX_PATH}/lib")
+set(${she_db}_LINK_TARGET   "she_db")
+
+
