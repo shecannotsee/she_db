d2817f019ffead5dc8bc4c5b8d1e3cea37dff571
shecannotsee <shecannotsee@163.com>
2024-02-05 16:01:57 +0800

    [refactor] move prefix_path in common_define.h

---
+++ src/common_define.h
@@ -0,0 +1,15 @@
+//
+// Created by shecannotsee on 24-2-5.
+//
+
+#ifndef COMMON_DEFINE_H
+#define COMMON_DEFINE_H
+
+#include <string>
+
+namespace she_db {
+const std::string prefix_path = "../store/";
+
+}
+
+#endif  // COMMON_DEFINE_H
