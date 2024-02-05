6d4c29d961573caef86af361362e4bb8275c3d10
shecannotsee <shecannotsee@163.com>
2024-02-05 17:49:38 +0800

    [feat] 1.Add path check 2.Add a function signature 3.boolean rw

--- src/data_type_operator.cpp
+++ src/data_type_operator.cpp
@@ -5,6 +5,166 @@
 #include "data_type_operator.h"
 
 namespace she_db {
-data_type_operator::data_type_operator(std::string&& file_path) : file_path_(std::move(file_path)) {
+
+data_type_operator::data_type_operator(std::string&& file_path) noexcept
+    : file_path_(std::move(file_path)), p_file_(fopen(file_path_.c_str(), "rb+"), &fclose) {
+  if (!p_file_) {
+    throw std::runtime_error("File pointer is null," + file_path_);
+  }
+}
+
+bool data_type_operator::read_boolean(const int offset) const noexcept {
+  union {
+    bool _1;
+    char _2[1];
+  } tmp;
+  // move ptr
+  if (fseek(p_file_.get(), offset, SEEK_SET) != 0) {
+    throw std::runtime_error("Error seeking to offset");
+  }
+  // read data
+  if (fread(&tmp._2, sizeof(char), boolean::size, p_file_.get()) != boolean::size) {
+    throw std::runtime_error("Error writing to file");
+  }
+  // init ptr
+  if (fseek(p_file_.get(), NULL, SEEK_SET) != 0) {
+    throw std::runtime_error("Error seeking to offset");
+  }
+  return tmp._1;
+}
+
+int8_t data_type_operator::read_int8(int offset) const noexcept {
+}
+int16_t data_type_operator::read_int16(int offset) const noexcept {
+}
+int32_t data_type_operator::read_int32(int offset) const noexcept {
+}
+int64_t data_type_operator::read_int64(int offset) const noexcept {
+}
+float data_type_operator::read_float32(int offset) const noexcept {
+}
+double data_type_operator::read_float64(int offset) const noexcept {
+}
+void data_type_operator::write_boolean(int offset, bool input_t) const noexcept {
+  union {
+    bool _1;
+    char _2[1];
+  } tmp;
+  tmp._1           = input_t;
+  const auto input = tmp._2;
+  // move ptr
+  if (fseek(p_file_.get(), offset, SEEK_SET) != 0) {
+    throw std::runtime_error("Error seeking to offset");
+  }
+  // write data
+  if (fwrite(input, sizeof(char), boolean::size, p_file_.get()) != boolean::size) {
+    throw std::runtime_error("Error writing to file");
+  }
+  // init ptr
+  if (fseek(p_file_.get(), NULL, SEEK_SET) != 0) {
+    throw std::runtime_error("Error seeking to offset");
+  }
+}
+void data_type_operator::write_int8(int offset, int8_t input) const noexcept {
+  // move ptr
+  if (fseek(p_file_.get(), offset, SEEK_SET) != 0) {
+    throw std::runtime_error("Error seeking to offset");
+  }
+  // write data
+  if (fwrite(&input, sizeof(char), int8::size, p_file_.get()) != int8::size) {
+    throw std::runtime_error("Error writing to file");
+  }
+  // init ptr
+  if (fseek(p_file_.get(), NULL, SEEK_SET) != 0) {
+    throw std::runtime_error("Error seeking to offset");
+  }
+}
+void data_type_operator::write_int16(int offset, int16_t input_t) const noexcept {
+  union {
+    int16_t _1;
+    char _2[2];
+  } tmp;
+  tmp._1           = input_t;
+  const auto input = tmp._2;
+  // move ptr
+  if (fseek(p_file_.get(), offset, SEEK_SET) != 0) {
+    throw std::runtime_error("Error seeking to offset");
+  }
+  // write data
+  if (fwrite(&input, sizeof(char), int16::size, p_file_.get()) != int16::size) {
+    throw std::runtime_error("Error writing to file");
+  }
+  // init ptr
+  if (fseek(p_file_.get(), NULL, SEEK_SET) != 0) {
+    throw std::runtime_error("Error seeking to offset");
+  }
+}
+void data_type_operator::write_int32(int offset, int32_t input_t) const noexcept {
+  union {
+    int32_t _1;
+    char _2[4];
+  } tmp;
+  tmp._1           = input_t;
+  const auto input = tmp._2;
+  // move ptr
+  if (fseek(p_file_.get(), offset, SEEK_SET) != 0) {
+    throw std::runtime_error("Error seeking to offset");
+  }
+  // write data
+  if (fwrite(&input, sizeof(char), int32::size, p_file_.get()) != int32::size) {
+    throw std::runtime_error("Error writing to file");
+  }
+  // init ptr
+  if (fseek(p_file_.get(), NULL, SEEK_SET) != 0) {
+    throw std::runtime_error("Error seeking to offset");
+  }
+}
+void data_type_operator::write_int64(int offset, int64_t input_t) const noexcept {
+  union {
+    int32_t _1;
+    char _2[8];
+  } tmp;
+  tmp._1           = input_t;
+  const auto input = tmp._2;
+  // move ptr
+  if (fseek(p_file_.get(), offset, SEEK_SET) != 0) {
+    throw std::runtime_error("Error seeking to offset");
+  }
+  // write data
+  if (fwrite(&input, sizeof(char), int64::size, p_file_.get()) != int64::size) {
+    throw std::runtime_error("Error writing to file");
+  }
+  // init ptr
+  if (fseek(p_file_.get(), NULL, SEEK_SET) != 0) {
+    throw std::runtime_error("Error seeking to offset");
+  }
+}
+void data_type_operator::write_float32(int offset, float input) const noexcept {
+  // move ptr
+  if (fseek(p_file_.get(), offset, SEEK_SET) != 0) {
+    throw std::runtime_error("Error seeking to offset");
+  }
+  // write data
+  if (fwrite(&input, sizeof(float), 1, p_file_.get()) != float32::size) {
+    throw std::runtime_error("Error writing to file");
+  }
+  // init ptr
+  if (fseek(p_file_.get(), NULL, SEEK_SET) != 0) {
+    throw std::runtime_error("Error seeking to offset");
+  }
+}
+void data_type_operator::write_float64(int offset, double input) const noexcept {
+  // move ptr
+  if (fseek(p_file_.get(), offset, SEEK_SET) != 0) {
+    throw std::runtime_error("Error seeking to offset");
+  }
+  // write data
+  if (fwrite(&input, sizeof(double), 1, p_file_.get()) != float64::size) {
+    throw std::runtime_error("Error writing to file");
+  }
+  // init ptr
+  if (fseek(p_file_.get(), NULL, SEEK_SET) != 0) {
+    throw std::runtime_error("Error seeking to offset");
+  }
 }
 }  // namespace she_db
--- src/data_type_operator.h
+++ src/data_type_operator.h
@@ -5,6 +5,8 @@
 #ifndef DATA_TYPES_RW_H
 #define DATA_TYPES_RW_H
 
+#include <memory>
+
 #include "common_define.h"
 
 namespace she_db {
@@ -21,30 +23,29 @@ class data_type_operator {
   data_type_operator(data_type_operator&&)            = delete;
   data_type_operator& operator=(data_type_operator&&) = delete;
   // ctor
-  explicit data_type_operator(std::string&& file_path);
+  explicit data_type_operator(std::string&& file_path) noexcept;
 
  private:
   std::string file_path_;
+  std::unique_ptr<FILE, decltype(&fclose)> p_file_;
 
+ private:
  public:  // basic data types
-  struct read {
-    bool boolean_ww();
-    int8_t int8_ww();
-    int16_t int16_ww();
-    int32_t int32_ww();
-    int64_t int64_ww();
-    float float32_ww();
-    double float64_ww();
-  };
-  struct write {
-    bool boolean_ww();
-    int8_t int8_ww();
-    int16_t int16_ww();
-    int32_t int32_ww();
-    int64_t int64_ww();
-    float float32_ww();
-    double float64_ww();
-  };
+  bool read_boolean(int offset) const noexcept;
+  int8_t read_int8(int offset) const noexcept;
+  int16_t read_int16(int offset) const noexcept;
+  int32_t read_int32(int offset) const noexcept;
+  int64_t read_int64(int offset) const noexcept;
+  float read_float32(int offset) const noexcept;
+  double read_float64(int offset) const noexcept;
+
+  void write_boolean(int offset, bool) const noexcept;
+  void write_int8(int offset, int8_t) const noexcept;
+  void write_int16(int offset, int16_t) const noexcept;
+  void write_int32(int offset, int32_t) const noexcept;
+  void write_int64(int offset, int64_t) const noexcept;
+  void write_float32(int offset, float) const noexcept;
+  void write_float64(int offset, double) const noexcept;
 
  public:  // complex data types
 };
