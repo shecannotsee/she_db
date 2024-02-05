08a81fd956ae1c59f036c109b1ab78d2946dcd66
shecannotsee <shecannotsee@163.com>
2024-02-05 16:04:28 +0800

    [test] unit conversion

--- test/t1_file_test.cpp
+++ test/t1_file_test.cpp
@@ -9,24 +9,21 @@
 
 namespace file {
 namespace {
-template <int file_size>
+template <long long file_size>
 bool check_file_size(const std::string& file_name) {
   struct stat file_stat;
 
-  // 使用 stat 获取文件信息
   if (stat(file_name.c_str(), &file_stat) != 0) {
-    // 如果获取失败，返回false
     return false;
   }
 
-  // 检查文件大小是否符合要求
+  // check size
   return file_stat.st_size == file_size;
 }
 }  // namespace
 
 bool create() {
   const auto file_name = she_db::createFile<5>();
-
-  return check_file_size<5>(she_db::prefix_path+file_name);
+  return check_file_size<5 * 1024>(she_db::prefix_path + file_name);
 }
 }  // namespace file
