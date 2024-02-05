7a36d32ee433798d462ccdc1b846a1d802a7f295
shecannotsee <shecannotsee@163.com>
2024-02-05 15:12:56 +0800

    [feat] create file function

--- src/create_file.h
+++ src/create_file.h
@@ -5,9 +5,30 @@
 #ifndef CREATE_FILE_H
 #define CREATE_FILE_H
 
+#include <cstdio>
+#include <stdexcept>
+#include <string>
+
 namespace she_db {
 
-class create_file {};
+template <int file_size_k>
+static void createFile() noexcept {
+  const std::string filePath = "../store/tableName.sheData";
+
+  FILE* pFile = fopen(filePath.c_str(), "wb+");
+
+  if (!pFile) {
+    throw std::runtime_error("Error opening file");
+  }
+
+  const size_t size = 1024 * file_size_k;
+
+  fseek(pFile, size - 1, SEEK_SET);
+  constexpr char buff = 0x00;
+  fwrite(&buff, sizeof(char), 1, pFile);
+
+  fclose(pFile);
+}
 
 }  // namespace she_db
 
