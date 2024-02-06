3b35a0688a5329aeeb2d517d12d7b40c163a65c2
shecannotsee <shecannotsee@163.com>
2024-02-06 15:24:57 +0800

    [feat] 1.Add operator_logger framework

---
+++ src/operator_logger.cpp
@@ -0,0 +1,7 @@
+//
+// Created by shecannotsee on 24-2-6.
+//
+
+#include "operator_logger.h"
+
+namespace she_db {}  // namespace she_db
---
+++ src/operator_logger.h
@@ -0,0 +1,14 @@
+//
+// Created by shecannotsee on 24-2-6.
+//
+
+#ifndef OPERATOR_LOGGER_H
+#define OPERATOR_LOGGER_H
+
+namespace she_db {
+
+class operator_logger {};
+
+}  // namespace she_db
+
+#endif  // OPERATOR_LOGGER_H
