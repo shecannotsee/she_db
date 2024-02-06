7ba4fa2c3da6ca2e9d6eedafe298d899dadeda05
shecannotsee <shecannotsee@163.com>
2024-02-06 15:23:55 +0800

    [feat] 1.Add create_file by file name

--- src/create_file.h
+++ src/create_file.h
@@ -22,7 +22,7 @@ namespace she_db {
  * \return file name
  */
 template <int file_size_k>
-static std::string createFile() noexcept {
+static std::string create_file() noexcept {
   const std::string file_name = []() {
     auto now           = std::chrono::system_clock::now();
     std::time_t time   = std::chrono::system_clock::to_time_t(now);
@@ -35,6 +35,50 @@ static std::string createFile() noexcept {
     oss << std::chrono::duration_cast<std::chrono::milliseconds>(now.time_since_epoch()).count() % 1000;  // 毫秒
     return oss.str();
   }();
+  const std::string file_path = prefix_path + file_name;
+
+  FILE* pFile = fopen(file_path.c_str(), "wb+");
+  if (!pFile) {
+    throw std::runtime_error("fopen error");
+  }
+
+  constexpr size_t size = 1024 * file_size_k / sizeof(char);
+
+  const auto fseek_expand_size = fseek(pFile, size - 1, SEEK_SET);
+  if (fseek_expand_size == -1) {
+    throw std::runtime_error("fseek error: fseek_expand_size failed.");
+  }
+  // Move the file pointer back to the beginning
+  const auto move_ptr_to_file_header = fseek(pFile, 0, SEEK_SET);
+  if (move_ptr_to_file_header == -1) {
+    throw std::runtime_error("fseek error: move_ptr_to_file_header failed.");
+  }
+
+  // init file content
+  constexpr char buff[size]{0x00};
+  const auto fwrite_to_init_file_bytes = fwrite(&buff, 1, size, pFile);
+  if (fwrite_to_init_file_bytes != size) {
+    const std::string error_message =
+        "fwrite error:" + std::to_string(fwrite_to_init_file_bytes) + "/" + std::to_string(size);
+    throw std::runtime_error(error_message);
+  }
+
+  const auto fclose_return_value_expect0 = fclose(pFile);
+  if (fclose_return_value_expect0 != 0) {
+    throw std::runtime_error("fclose error");
+  }
+
+  return file_name;
+}
+
+/**
+ * \brief Use the template to create a file of the appropriate size given the numbers
+ * \tparam file_size_k file size,in k
+ * \param file_name file name
+ * \return file name
+ */
+template <int file_size_k>
+static std::string create_file(std::string file_name) noexcept {
   const std::string file_path = prefix_path + file_name;
 
   FILE* pFile = fopen(file_path.c_str(), "wb+");
