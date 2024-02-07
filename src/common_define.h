256c47d5287289ef6467e44ba1f0226c1aaae065
shecannotsee <shecannotsee@163.com>
2024-02-07 15:27:25 +0800

    [feat] Add make_unique

--- src/common_define.h
+++ src/common_define.h
@@ -5,7 +5,17 @@
 #ifndef COMMON_DEFINE_H
 #define COMMON_DEFINE_H
 
+#include <memory>
 #include <string>
+
+#if __cplusplus >= 201103L && __cplusplus < 201402L
+namespace std {
+template <typename T, typename... Args>
+std::unique_ptr<T> make_unique(Args&&... args) {
+  return std::unique_ptr<T>(new T(std::forward<Args>(args)...));
+}
+}  // namespace std
+#endif
 
 namespace she_db {
 const std::string prefix_path       = "../store/";
@@ -29,13 +39,13 @@ struct integral_constant {
   using type                          = real_type;
 };
 
-using she_boolean = integral_constant<db_data_type::boolean, bool,    1>;
-using she_int8    = integral_constant<db_data_type::int8,    int8_t,  1>;
-using she_int16   = integral_constant<db_data_type::int16,   int16_t, 2>;
-using she_int32   = integral_constant<db_data_type::int32,   int32_t, 4>;
-using she_int64   = integral_constant<db_data_type::int64,   int64_t, 8>;
-using she_float32 = integral_constant<db_data_type::float32, float,   4>;
-using she_float64 = integral_constant<db_data_type::float64, double,  8>;
+using she_boolean = integral_constant<db_data_type::boolean, bool, 1>;
+using she_int8    = integral_constant<db_data_type::int8, int8_t, 1>;
+using she_int16   = integral_constant<db_data_type::int16, int16_t, 2>;
+using she_int32   = integral_constant<db_data_type::int32, int32_t, 4>;
+using she_int64   = integral_constant<db_data_type::int64, int64_t, 8>;
+using she_float32 = integral_constant<db_data_type::float32, float, 4>;
+using she_float64 = integral_constant<db_data_type::float64, double, 8>;
 
 }  // namespace she_db
 
