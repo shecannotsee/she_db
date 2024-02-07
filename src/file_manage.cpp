fac357cddb5fa932ab115c309874f1b0efc038b7
shecannotsee <shecannotsee@163.com>
2024-02-07 15:27:13 +0800

    [feat] Add self-checking

--- src/file_manage.cpp
+++ src/file_manage.cpp
@@ -14,5 +14,30 @@ file_manage::file_manage() : read_(nullptr), write_(nullptr) {
   if (!detail::file::exist(initial_file_path)) {
     create_file<1>(initial_file_path);
   }
+  bool init = false;
+  /* self-checking */ {
+    const auto read_something = std::make_unique<detail::read>(initial_file_path);
+    // 1(type) + 1(name size) + 14
+    const uint32_t read_size = 1 + 1 + initial_file_name.size();
+    // exec
+    auto content = read_something->get(0, read_size);
+    if (static_cast<file_type>(content.at(0)) != file_type::initial) {
+      goto init_false;
+    }
+    if (static_cast<uint8_t>(content.at(1)) != 0x0e) {
+      goto init_false;
+    }
+    init = true;
+
+  init_false:;
+  }
+  if (!init) {
+    const auto write_something = std::make_unique<detail::write>(initial_file_path);
+    std::vector<uint8_t> initial_file_info{0x01, 0x0e};
+    for (const auto& b : initial_file_name) {
+      initial_file_info.emplace_back(static_cast<uint8_t>(b));
+    }
+    write_something->content(initial_file_info, 0);
+  }
 }
 }  // namespace she_db
